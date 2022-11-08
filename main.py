d25700bec208c774872819da046c642f12f4e54e
Sewa <sewabobrov04@gmail.com>
2022-11-08 21:01:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.dispatcher.filters import Command, Text
+from aiogram import Bot, Dispatcher, executor, types
+import psycopg2
